f7b1c8544313b3bd92ba24860d65f6214bcd6ddd
GreggtheDev <greggthedev@gmail.com>
2024-08-20 15:05:36 -0400

    Implement SpecialOfferService class to manage promotional offers
    - Added methods to add new special offers, remove existing offers, and retrieve all special offers using SpecialOfferDao.
    - Integrated SpecialOfferDao to handle database interactions for promotional offers.
    - Facilitated the management of special offers within the service layer of the restaurant management system.

---
+++ src/main/java/com/restaurant/service/SpecialOfferService.java
@@ -0,0 +1,52 @@
+package com.restaurant.service;
+
+import com.restaurant.dao.SpecialOfferDao;
+import com.restaurant.model.SpecialOffer;
+
+import java.util.List;
+
+/**
+ * The SpecialOfferService class provides business logic for managing special offers
+ * in the restaurant management system. It interacts with the SpecialOfferDao to
+ * perform CRUD operations on special offers.
+ */
+public class SpecialOfferService {
+
+    private final SpecialOfferDao specialOfferDao;
+
+    /**
+     * Constructs a SpecialOfferService instance and initializes the SpecialOfferDao
+     * for interacting with the database.
+     */
+    public SpecialOfferService() {
+        this.specialOfferDao = new SpecialOfferDao();
+    }
+
+    /**
+     * Adds a new special offer to the system by calling the DAO layer.
+     *
+     * @param offer The SpecialOffer object containing the details to be added.
+     */
+    public void addSpecialOffer(SpecialOffer offer) {
+        specialOfferDao.addSpecialOffer(offer);
+    }
+
+    /**
+     * Removes an existing special offer from the system by calling the DAO layer.
+     *
+     * @param offerId The ID of the special offer to be removed.
+     */
+    public void removeSpecialOffer(int offerId) {
+        specialOfferDao.removeSpecialOffer(offerId);
+    }
+
+    /**
+     * Retrieves all special offers from the system by calling the DAO layer.
+     *
+     * @return A list of all SpecialOffer objects.
+     */
+    public List<SpecialOffer> getAllSpecialOffers() {
+        return specialOfferDao.getAllSpecialOffers();
+    }
+}
+
